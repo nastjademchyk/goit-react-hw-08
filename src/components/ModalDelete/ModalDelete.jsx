e64cfa0505248d68fd92dde00643d888f4fc864a
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-12-10 11:09:14 +0100

    added backdrop function to modalDelete overlay

--- src/components/ModalDelete/ModalDelete.jsx
+++ src/components/ModalDelete/ModalDelete.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import s from "./ModalDelete.module.css";
 
 export const ModalDelete = ({ onConfirm, onCancel }) => {
+  const handleBackDropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onCancel();
+    }
+  };
+
   return (
-    <div className={s.modalOverlay}>
+    <div className={s.modalOverlay} onClick={handleBackDropClick}>
       <div className={s.modal}>
         <p className={s.text}>Are you sure you want to delete this contact?</p>
         <div className={s.buttons}>
